a6d40a3dacea623e5139617b555f7c52e4316c16
zranl <43489608+zranl@users.noreply.github.com>
2021-03-15 18:30:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,29 +7,29 @@
  */
 // 在head 中 加载 必要静态
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/sunpma/goIndex/css/mdui.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/zranl/goIndex/css/mdui.min.css">'
 );
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/sunpma/goIndex/css/nexmoe.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/zranl/goIndex/css/nexmoe.min.css">'
 );
 // markdown支持
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/sunpma/goIndex/js/markdown-it.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/zranl/goIndex/js/markdown-it.min.js"></script>'
 );
 document.write(
   "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}</style>"
 );
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/sunpma/goIndex/css/DPlayer.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/zranl/goIndex/css/DPlayer.min.css">'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/sunpma/goIndex/js/DPlayer.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/zranl/goIndex/js/DPlayer.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/sunpma/goIndex/js/hls.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/zranl/goIndex/js/hls.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/sunpma/goIndex/js/flv.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/zranl/goIndex/js/flv.min.js"></script>'
 );
 
 // 初始化页面，并载入必要资源
@@ -39,11 +39,11 @@ function init() {
   var html = `
 <header class="nexmoe-nav">
     <div class="navSize">
-			<a href="/"><img class="avatar" src="https://cdn.jsdelivr.net/gh/sunpma/goIndex/images/icon.png"/></a>
+			<a href="/"><img class="avatar" src="https://cdn.jsdelivr.net/gh/zranl/goIndex/images/icon.png"/></a>
 			<div class="nav_menu">
 				<ul class="menu_ul">
-					<li class="menu_li"><a href="https://sunpma.com" target="_blank">博客</a></li>
-					<li class="menu_li"><a href="https://coffee.sunpma.com" target="_blank">导航</a></li>
+					<li class="menu_li"><a href="https://FRDS.lyuan.workers.dev" target="_blank">FRDS</a></li>
+					<li class="menu_li"><a href="https://javbus.lyuan.workers.dev" target="_blank">JavBus</a></li>
 				</ul>
 				<div class="nav_icon" ></div>
 			</div>
@@ -275,8 +275,8 @@ function file_code(path) {
 </div>
 <a href="${href}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 
-<script src="//cdn.jsdelivr.net/gh/sunpma/goIndex/js/ace.js"></script>
-<script src="//cdn.jsdelivr.net/gh/sunpma/goIndex/js/ext-language_tools.js"></script>
+<script src="//cdn.jsdelivr.net/gh/zranl/goIndex/js/ace.js"></script>
+<script src="//cdn.jsdelivr.net/gh/zranl/goIndex/js/ext-language_tools.js"></script>
 	`;
   $("#content").html(content);
 
